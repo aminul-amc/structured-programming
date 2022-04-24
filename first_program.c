9227d3f8aeba8fe55b990dd55fbd21b350ef96c0
aminul-amc <02islamaminul@gmail.com>
2022-04-24 14:32:54 +0600

    ok

---
+++ first_program.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int price ;
+    int marks=55;
+    float tem;
+    price =34+14;
+    printf("Price is %d\n",price);
+    price =5;
+    price=marks-price;
+
+    printf("Price is %d\n",price);
+}
